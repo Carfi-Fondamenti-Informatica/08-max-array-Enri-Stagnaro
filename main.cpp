ec62affb6ba212d43038b2783e97beb021cb1d35
Enri-Stagnaro <115564859+Enri-Stagnaro@users.noreply.github.com>
2022-12-12 19:13:32 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,16 @@
 #include <iostream>
+#include "lib.h"
 
-int main(){
-  return 0;
+using namespace std;
+
+
+int main() {
+    int n;
+    cin >> n;
+    float numeri[n];
+    for (int i = 0; i < n; i++) {
+        cin >> numeri[i];
+    }
+    cout << maggiore(numeri, n);
+    return 0;
 }
