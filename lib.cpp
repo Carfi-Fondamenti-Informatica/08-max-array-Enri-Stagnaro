48fdaf585ed4297ec01281c314b5d4e2d60ef60d
Enri-Stagnaro <115564859+Enri-Stagnaro@users.noreply.github.com>
2022-12-12 19:12:11 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,14 @@
 
+#include <iostream>
+#include "lib.h"
+
+float maggiore(float numero[], int n) {
+
+    float cont = numero[0];
+    for (int i = 0; i < n; i++) {
+        if (numero[i] > cont) {
+            cont = numero[i];
+        }
+    }
+    return cont;
+}
